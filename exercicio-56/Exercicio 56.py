dd04884aa0646742f41cebd944e41fe0fdc561f9
mangerotte <mangerotte@gmail.com>
2022-08-28 18:43:23 -0300

    Exercicio

--- exercicio-56/Exercicio 56.py
+++ exercicio-56/Exercicio 56.py
@@ -2,10 +2,12 @@ active = True
 s = 0
 
 while active:
+
     n = int(input('Digite um numero: '))
-    s = n + s
 
     if n == 1111:
-        active = False
+        break
+
+    s = n + s
 
 print(f'A soma total dos numeros digitado foi de {s}')
